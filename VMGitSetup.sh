677328dd7654993153f6d825f10c8f548f0766cf
Lauren Darlaston <80410139+AelinaXY@users.noreply.github.com>
2023-03-01 15:52:36 +0000

    Remove broken code

--- VMGitSetup.sh
+++ VMGitSetup.sh
@@ -2,5 +2,3 @@ git config --global user.name "AelinaXY"
 git config --global user.email "Lauren@2infinity.net"
 ssh-keygen -t rsa -f ~/.ssh/id_rsa <<< y
 cat ~/.ssh/id_rsa.pub >> rsapublic.txt
-curl -H "Authorization: token REPLACE" 
-	--data '{"title":"VM `date +%d%m%Y`","key":"`cat ~/.ssh/id_rsa.pub`"}' https://api.github.com/user/keys
